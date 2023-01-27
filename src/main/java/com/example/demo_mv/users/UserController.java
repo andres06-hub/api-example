4e0ffccabecdc0dba90fbde35f40e7eb66e42c13
andres06-hub <andressierrarojas7@gmail.com>
2023-01-27 00:08:36 -0500

    CREATED: controller for Delete user

--- src/main/java/com/example/demo_mv/users/UserController.java
+++ src/main/java/com/example/demo_mv/users/UserController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo_mv.dto.ResponseDto;
@@ -15,6 +14,7 @@ import jakarta.annotation.Resource;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import lombok.extern.slf4j.Slf4j;
@@ -39,5 +39,12 @@ public class UserController {
     if (result.status == true) return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto(true, "Exist User!", result.data));
     log.info("User created: "+result.data);
     return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto(true, "Created User!", null));
+  }
+
+  @DeleteMapping("/user/{id}")
+  public ResponseEntity<ResponseDto> delUser(@PathVariable("id") String id) {
+    ReturnTypeDto result = service.deleteUser(id);
+    if (result.status == false) return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseDto(true, "User does not exist!", null));
+    return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto(true,"Deleted User!",null));
   }
 }
