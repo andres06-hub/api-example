8b0d23bfa4e22a053bfbce22054e326f0835ef8b
andres06-hub <andressierrarojas7@gmail.com>
2023-01-27 00:27:20 -0500

    ADD: controller get one user

--- src/main/java/com/example/demo_mv/users/UserController.java
+++ src/main/java/com/example/demo_mv/users/UserController.java
@@ -29,13 +29,23 @@ public class UserController {
 
   @GetMapping("/users")
   public ResponseEntity<ResponseDto> getAll() {
-    List<UserEntity> users = service.listUsers();
+    List<UserEntity> users = this.service.listUsers();
     return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto(true, "USERS: ", users));
+  }
+
+  @GetMapping("/user/{id}")
+  public ResponseEntity<ResponseDto> getUser(@PathVariable("id") String id){
+    //TODO: CREATE FACTORY FOR ID, String -> Long
+    Long _id = (long) Integer.parseInt(id);
+    UserEntity result = this.service.findById(_id);
+    if (result == null) return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseDto(true,"User not found",result));
+    log.info("USER FOUND!");
+    return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto(true, "user", result));
   }
 
   @PostMapping("/user")
   public ResponseEntity<ResponseDto> createUser(@RequestBody UserEntity user) {
-    ReturnTypeDto result = service.saveUser(user);
+    ReturnTypeDto result = this.service.saveUser(user);
     if (result.status == true) return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto(true, "Exist User!", result.data));
     log.info("User created: "+result.data);
     return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto(true, "Created User!", null));
@@ -43,7 +53,9 @@ public class UserController {
 
   @DeleteMapping("/user/{id}")
   public ResponseEntity<ResponseDto> delUser(@PathVariable("id") String id) {
-    ReturnTypeDto result = service.deleteUser(id);
+    //TODO: CREATE FACTORY FOR ID, String -> Long
+    Long _id = (long) Integer.parseInt(id);
+    ReturnTypeDto result = this.service.deleteUser(_id);
     if (result.status == false) return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseDto(true, "User does not exist!", null));
     return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto(true,"Deleted User!",null));
   }
