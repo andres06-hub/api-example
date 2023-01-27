3066eef9ad21f4171a3518a882ae1c48f68dcd23
andres06-hub <andressierrarojas7@gmail.com>
2023-01-26 23:21:31 -0500

    CHANGES: methos

--- src/main/java/com/example/demo_mv/users/interfaces/UserInterface.java
+++ src/main/java/com/example/demo_mv/users/interfaces/UserInterface.java
@@ -2,12 +2,14 @@ package com.example.demo_mv.users.interfaces;
 
 import java.util.List;
 
+import com.example.demo_mv.dto.ReturnTypeDto;
 import com.example.demo_mv.users.UserEntity;
 
 public interface UserInterface {
   
   public List<UserEntity> listUsers();
   public UserEntity findById(Long id);
-  public UserEntity saveUser(UserEntity user);
+  public UserEntity findByMail(String mail);
+  public ReturnTypeDto saveUser(UserEntity user);
   public void delete(UserEntity user);
 }
