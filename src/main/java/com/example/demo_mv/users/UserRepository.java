7a924904834c80e035068bcf8b9faacd57e61b9c
andres06-hub <andressierrarojas7@gmail.com>
2023-01-26 23:19:23 -0500

    ADD: new query

--- src/main/java/com/example/demo_mv/users/UserRepository.java
+++ src/main/java/com/example/demo_mv/users/UserRepository.java
@@ -1,5 +1,10 @@
 package com.example.demo_mv.users;
 
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<UserEntity, Long> {}
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+  @Query("SELECT u FROM UserEntity u WHERE u.email = ?1 ")
+  public UserEntity findByMail(String mail);
+}
